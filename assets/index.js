0f942d70655a52978b09141ce5a2c384af3b1c0b
Mostafa Maki <mostafa_m9@yahoo.com>
2022-08-31 19:22:43 -0700

    installed dependencies and updated prompt questions

--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,92 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const markdown = require('./generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'input',
+      name: 'location',
+      message: 'Where are you from?',
+    },
+    {
+      type: 'input',
+      name: 'hobby',
+      message: 'What is your favorite hobby?',
+    },
+    {
+      type: 'input',
+      name: 'food',
+      message: 'What is your favorite food?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL.',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
+
+fs.writeFile('README.md', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created README.md!')
+    );
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
 init();
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'input',
+      name: 'location',
+      message: 'Where are you from?',
+    },
+    {
+      type: 'input',
+      name: 'hobby',
+      message: 'What is your favorite hobby?',
+    },
+    {
+      type: 'input',
+      name: 'food',
+      message: 'What is your favorite food?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL.',
+    },
+  ])
+  .then((answers) => {
+    const htmlPageContent = badmath.generateHTML(answers);
+
+    fs.writeFile('README.md', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created README.md!')
+    );
+  });
--- assets/utils/generateMarkdown.js
+++ assets/utils/generateMarkdown.js
@@ -13,7 +13,52 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Description from ${data.location}
+  Provide a short description ${data.github} explaining the what, why, and how of your project. Use the following questions as a guide:
 
+    - What was your motivation?
+    - Why did you build this project? (Note: the answer is not${data.linkedin} "Because it was a homework assignment.")
+    - What problem does it solve?
+    - What did you learn?
+
+## Table of Contents
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+
+  ## Installation
+
+  What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+  ## Usage
+
+  Provide instructions and examples for use. Include screenshots as needed.
+
+  ## License
+
+  The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+
+  ---
+
+
+  ## Contributing
+
+  If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+
+  ## Tests
+
+  Go the extra mile and write tests for your application. Then provide examples on how to run them here.
+
+
+  ## Questions
 `;
 }
 
